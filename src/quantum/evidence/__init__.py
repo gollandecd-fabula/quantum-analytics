491568a2378da5825f40156725e64dc0d73ca8d1
gollandecd-fabula <gollandecd@gmail.com>
2026-06-28 19:56:17 +0500

    B3: add runtime evidence verifier package

---
+++ src/quantum/evidence/__init__.py
@@ -0,0 +1,19 @@
+from .verification import (
+    EDGE_SIGNATURES,
+    canonical_graph_hash,
+    canonical_snapshot_hash,
+    diagnose_evidence_chain,
+    diagnose_metric_snapshot,
+    verify_evidence_chain,
+    verify_metric_snapshot,
+)
+
+__all__ = [
+    "EDGE_SIGNATURES",
+    "canonical_graph_hash",
+    "canonical_snapshot_hash",
+    "diagnose_evidence_chain",
+    "diagnose_metric_snapshot",
+    "verify_evidence_chain",
+    "verify_metric_snapshot",
+]
