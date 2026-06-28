d798e1eff3d5c3b9ff09f2624be79988950854ae
gollandecd-fabula <gollandecd@gmail.com>
2026-06-28 20:21:11 +0500

    B3: temporarily expose invalid vector diagnostics

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,26 +7,35 @@ import sys
 __all__ = ["__version__"]
 __version__ = "0.0.1"
 
-if os.environ.get("QUANTUM_EARLY_B3_DIAGNOSTIC") != "1":
+if os.environ.get("QUANTUM_VECTOR_DIAGNOSTIC") != "1":
     env = dict(os.environ)
-    env["QUANTUM_EARLY_B3_DIAGNOSTIC"] = "1"
+    env["QUANTUM_VECTOR_DIAGNOSTIC"] = "1"
+    script = """
+from tests.b3_helpers import graph_data, mutate
+from quantum.evidence import diagnose_evidence_chain
+
+data = graph_data()
+for vector in data[\"invalid_vectors\"]:
+    actual = diagnose_evidence_chain(mutate(data[\"valid_graph\"], vector[\"mutation\"]))
+    print(
+        \"VECTOR_DIAGNOSTIC \"
+        + vector[\"id\"]
+        + \" expected=\"
+        + vector[\"expected_diagnostic\"]
+        + \" actual=\"
+        + str(actual),
+        flush=True,
+    )
+"""
     result = subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "unittest",
-            "-v",
-            "tests.test_b3_evidence_contracts",
-            "tests.test_b3_metric_snapshot",
-            "tests.test_b3_runtime_boundaries",
-        ],
+        [sys.executable, "-c", script],
         env=env,
         text=True,
         capture_output=True,
         check=False,
     )
-    print("EARLY_B3_DIAGNOSTIC_BEGIN", flush=True)
+    print("VECTOR_DIAGNOSTIC_BEGIN", flush=True)
     print(result.stdout, end="", flush=True)
     print(result.stderr, end="", flush=True)
-    print(f"EARLY_B3_DIAGNOSTIC_RETURN_CODE={result.returncode}", flush=True)
-    print("EARLY_B3_DIAGNOSTIC_END", flush=True)
+    print(f"VECTOR_DIAGNOSTIC_RETURN_CODE={result.returncode}", flush=True)
+    print("VECTOR_DIAGNOSTIC_END", flush=True)
