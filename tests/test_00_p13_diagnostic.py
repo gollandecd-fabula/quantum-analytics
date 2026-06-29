0b788889b393b015721e691d2cd8c3f772917a43
gollandecd-fabula <gollandecd@gmail.com>
2026-06-30 04:30:06 +0500

    [P1.3] Remove temporary diagnostic suite

--- tests/test_00_p13_diagnostic.py
+++
@@ -1,9 +0,0 @@
-from tests.test_p13_canonical_ledger import P13CanonicalLedgerTests
-from tests.test_p13_event_integrity import P13EventIntegrityTests
-from tests.test_p13_review_regressions import P13ReviewRegressionTests
-
-__all__ = [
-    "P13CanonicalLedgerTests",
-    "P13EventIntegrityTests",
-    "P13ReviewRegressionTests",
-]
