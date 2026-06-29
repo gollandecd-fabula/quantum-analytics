29e431ce7b5f26ed6299fa0931e530fe9aba6f01
gollandecd-fabula <gollandecd@gmail.com>
2026-06-30 02:24:30 +0500

    [P1.1] Export immutable upload receipt contracts

--- src/quantum/ingestion/__init__.py
+++ src/quantum/ingestion/__init__.py
@@ -1,8 +1,12 @@
 from .fingerprints import semantic_fingerprint, structural_fingerprint
+from .receipts import ImmutableUploadReceipt, IngestionError, UploadReceiptRegistry
 from .schema_registry import SchemaDetection, detect_csv_schema
 
 __all__ = [
+    "ImmutableUploadReceipt",
+    "IngestionError",
     "SchemaDetection",
+    "UploadReceiptRegistry",
     "detect_csv_schema",
     "semantic_fingerprint",
     "structural_fingerprint",
