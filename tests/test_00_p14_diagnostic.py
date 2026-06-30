243e1004309ece5af86d6dbea6f968e0f66faabf
gollandecd-fabula <gollandecd@gmail.com>
2026-06-30 16:21:21 +0500

    [P1.4] Remove temporary diagnostic suite

--- tests/test_00_p14_diagnostic.py
+++
@@ -1,7 +0,0 @@
-from tests.test_p14_reporting_adversarial import P14ReportingAdversarialTests
-from tests.test_p14_reporting_runtime import P14ReportingRuntimeTests
-
-__all__ = [
-    "P14ReportingAdversarialTests",
-    "P14ReportingRuntimeTests",
-]
