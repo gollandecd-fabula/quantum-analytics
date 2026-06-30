edfda042e027589b8c02418d16f1223fad114364
gollandecd-fabula <gollandecd@gmail.com>
2026-06-30 16:18:42 +0500

    [P1.4] Add temporary early diagnostic suite

---
+++ tests/test_00_p14_diagnostic.py
@@ -0,0 +1,7 @@
+from tests.test_p14_reporting_adversarial import P14ReportingAdversarialTests
+from tests.test_p14_reporting_runtime import P14ReportingRuntimeTests
+
+__all__ = [
+    "P14ReportingAdversarialTests",
+    "P14ReportingRuntimeTests",
+]
