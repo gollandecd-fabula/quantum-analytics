ea68b279d447f7221fa1ebc27594a9b124fb97d0
gollandecd-fabula <gollandecd@gmail.com>
2026-07-01 23:14:05 +0500

    fix(p16): activate compatible schema reference validation

--- src/quantum/ingestion/admission.py
+++ src/quantum/ingestion/admission.py
@@ -1,1 +1,5 @@
 from .admission_v2 import *
+from ._admission_reference_contract import apply_reference_contract as _apply_reference_contract
+
+_apply_reference_contract()
+del _apply_reference_contract
