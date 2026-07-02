07b32cf1776981dc81163117d1c1e8f82ea1c44f
gollandecd-fabula <gollandecd@gmail.com>
2026-07-02 22:14:56 +0500

    refactor(b1b-rescue): switch to split evaluation facade

--- src/quantum/finance/_rules_hardening.py
+++ src/quantum/finance/_rules_hardening.py
@@ -1,4 +1,4 @@
-from ._rules_evaluate_facade import evaluate_resolved_rule
+from ._rules_evaluate_v2 import evaluate_resolved_rule
 from ._rules_resolve_facade import resolve_rule
 
 __all__ = ["evaluate_resolved_rule", "resolve_rule"]
