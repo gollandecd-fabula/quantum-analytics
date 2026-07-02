e834bfd367683a1b09e80abff2ce077501053e7b
gollandecd-fabula <gollandecd@gmail.com>
2026-07-03 03:43:40 +0500

    Add R20 local pilot package exports

---
+++ src/quantum/pilot/__init__.py
@@ -0,0 +1,13 @@
+from .runtime import (
+    LocalPilotExecutionError,
+    LocalPilotScope,
+    execute_local_read_only_pilot,
+    finance_result_snapshot,
+)
+
+__all__ = [
+    "LocalPilotExecutionError",
+    "LocalPilotScope",
+    "execute_local_read_only_pilot",
+    "finance_result_snapshot",
+]
