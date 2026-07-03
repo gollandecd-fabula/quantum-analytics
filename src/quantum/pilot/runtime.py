94b4362e0b32c3349c6f4e0e7695f3af3255a864
gollandecd-fabula <gollandecd@gmail.com>
2026-07-03 10:00:02 +0500

    feat(pilot): export operational runner API

--- src/quantum/pilot/runtime.py
+++ src/quantum/pilot/runtime.py
@@ -1,10 +1,16 @@
 from ._bindings import finance_result_snapshot
 from ._execution import execute_local_read_only_pilot
 from ._scope import LocalPilotExecutionError, LocalPilotScope
+from .purge import purge_workspace
+from .runner import run_manifest
+from .validation import validate_manifest
 
 __all__ = [
     "LocalPilotExecutionError",
     "LocalPilotScope",
     "execute_local_read_only_pilot",
     "finance_result_snapshot",
+    "purge_workspace",
+    "run_manifest",
+    "validate_manifest",
 ]
