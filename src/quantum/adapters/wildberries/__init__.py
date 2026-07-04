deb927bd68884f2d1deaa8347602648c8b7b79a6
gollandecd-fabula <gollandecd@gmail.com>
2026-07-05 00:05:14 +0500

    Expose WB source bridge without breaking synthetic adapter

--- src/quantum/adapters/wildberries/__init__.py
+++ src/quantum/adapters/wildberries/__init__.py
@@ -1,3 +1,17 @@
+from .source_bridge import (
+    BRIDGE_SCHEMA_VERSION,
+    SUPPLIER_GOODS_SOURCE_TYPE,
+    WbSourceBridgeError,
+    bridge_admitted_xlsx,
+)
 from .synthetic import NormalizedRow, normalize_row, validate_row
 
-__all__ = ["NormalizedRow", "normalize_row", "validate_row"]
+__all__ = [
+    "BRIDGE_SCHEMA_VERSION",
+    "SUPPLIER_GOODS_SOURCE_TYPE",
+    "WbSourceBridgeError",
+    "bridge_admitted_xlsx",
+    "NormalizedRow",
+    "normalize_row",
+    "validate_row",
+]
