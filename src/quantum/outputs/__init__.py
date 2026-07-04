f3d3ea184123cc6272b2c534d969fe053c647b74
gollandecd-fabula <gollandecd@gmail.com>
2026-07-05 01:59:29 +0500

    Expose immutable output verification API

--- src/quantum/outputs/__init__.py
+++ src/quantum/outputs/__init__.py
@@ -1,5 +1,6 @@
 from .dashboard import render_dashboard_html
 from .local_bundle import (
+    EXPECTED_XLSX_SHEETS,
     LOCAL_OUTPUT_BUNDLE_SCHEMA_VERSION,
     LOCAL_OUTPUT_MANIFEST_SCHEMA_VERSION,
     OutputBundleError,
@@ -7,9 +8,14 @@ from .local_bundle import (
     render_xlsx_report,
     validate_local_output_bundle,
 )
-from .writer import write_local_output_bundle
+from .writer import (
+    validate_local_output_manifest,
+    verify_local_output_directory,
+    write_local_output_bundle,
+)
 
 __all__ = [
+    "EXPECTED_XLSX_SHEETS",
     "LOCAL_OUTPUT_BUNDLE_SCHEMA_VERSION",
     "LOCAL_OUTPUT_MANIFEST_SCHEMA_VERSION",
     "OutputBundleError",
@@ -17,5 +23,7 @@ __all__ = [
     "render_dashboard_html",
     "render_xlsx_report",
     "validate_local_output_bundle",
+    "validate_local_output_manifest",
+    "verify_local_output_directory",
     "write_local_output_bundle",
 ]
