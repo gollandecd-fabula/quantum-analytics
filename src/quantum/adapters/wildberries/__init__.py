77352837dc370e9d425086a789ff1c776aed4de0
gollandecd-fabula <gollandecd@gmail.com>
2026-07-05 00:27:43 +0500

    Expose direct WB detailed XLSX bridge API

--- src/quantum/adapters/wildberries/__init__.py
+++ src/quantum/adapters/wildberries/__init__.py
@@ -4,6 +4,11 @@ from .detailed_financial import (
     WbDetailedFinancialError,
     normalize_detailed_financial_rows,
 )
+from .detailed_xlsx import (
+    DETAILED_XLSX_SCHEMA_VERSION,
+    WbDetailedXlsxError,
+    bridge_detailed_financial_xlsx,
+)
 from .source_bridge import (
     BRIDGE_SCHEMA_VERSION,
     SUPPLIER_GOODS_SOURCE_TYPE,
@@ -16,10 +21,13 @@ __all__ = [
     "BRIDGE_SCHEMA_VERSION",
     "DETAILED_FINANCIAL_SCHEMA_VERSION",
     "DETAILED_FINANCIAL_SOURCE_TYPE",
+    "DETAILED_XLSX_SCHEMA_VERSION",
     "SUPPLIER_GOODS_SOURCE_TYPE",
     "WbDetailedFinancialError",
+    "WbDetailedXlsxError",
     "WbSourceBridgeError",
     "bridge_admitted_xlsx",
+    "bridge_detailed_financial_xlsx",
     "normalize_detailed_financial_rows",
     "NormalizedRow",
     "normalize_row",
