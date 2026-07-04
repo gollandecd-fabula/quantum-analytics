622f7a84666928d1b4560ead2dfe3438b8193ab0
gollandecd-fabula <gollandecd@gmail.com>
2026-07-05 00:20:20 +0500

    Expose WB detailed financial mapper API

--- src/quantum/adapters/wildberries/__init__.py
+++ src/quantum/adapters/wildberries/__init__.py
@@ -1,3 +1,9 @@
+from .detailed_financial import (
+    DETAILED_FINANCIAL_SCHEMA_VERSION,
+    DETAILED_FINANCIAL_SOURCE_TYPE,
+    WbDetailedFinancialError,
+    normalize_detailed_financial_rows,
+)
 from .source_bridge import (
     BRIDGE_SCHEMA_VERSION,
     SUPPLIER_GOODS_SOURCE_TYPE,
@@ -8,9 +14,13 @@ from .synthetic import NormalizedRow, normalize_row, validate_row
 
 __all__ = [
     "BRIDGE_SCHEMA_VERSION",
+    "DETAILED_FINANCIAL_SCHEMA_VERSION",
+    "DETAILED_FINANCIAL_SOURCE_TYPE",
     "SUPPLIER_GOODS_SOURCE_TYPE",
+    "WbDetailedFinancialError",
     "WbSourceBridgeError",
     "bridge_admitted_xlsx",
+    "normalize_detailed_financial_rows",
     "NormalizedRow",
     "normalize_row",
     "validate_row",
