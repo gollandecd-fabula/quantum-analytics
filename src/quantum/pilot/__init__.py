dde8f9caee5d854e0cae20ae1e44e78e69f09a5d
gollandecd-fabula <gollandecd@gmail.com>
2026-07-04 13:52:29 +0500

    Activate Windows compatibility for public local pilot API

--- src/quantum/pilot/__init__.py
+++ src/quantum/pilot/__init__.py
@@ -1,3 +1,10 @@
 from .local_runner import LocalPilotError, run_local_pilot
+from .windows_runner import install_windows_compatibility
 
-__all__ = ["LocalPilotError", "run_local_pilot"]
+install_windows_compatibility()
+
+__all__ = [
+    "LocalPilotError",
+    "install_windows_compatibility",
+    "run_local_pilot",
+]
